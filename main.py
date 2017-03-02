efe3894ef7e8e7ef98513fe359e5dadf8de1c876
Dina1993 <dinaibrahim8.di@gmail.com>
2017-03-02 14:42:22 +0200

    new main with login

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from tornado import web,ioloop
 from handlers.ajax import APIHandler
+from handlers.sse import EventHandler,SSEHandler
+from handlers.login import ChatHandler,WSHandler,Login
+
 
 class MainHandler(web.RequestHandler):
     def get(self):
-        self.render("templates/index.html")
+        self.render("templates/chat.html")
 
 app = web.Application([
-		(r"/", MainHandler),
-        (r"/myapi", APIHandler)
+	(r"/", MainHandler),
+        (r"/myapi", APIHandler),
+        (r"/sse", SSEHandler),
+        (r"/event", EventHandler),
+        (r"/chat", ChatHandler),
+        (r"/log", Login),
+        (r"/ws", WSHandler)
 	],static_path='static',debug=True)
 
 app.listen(8888)
