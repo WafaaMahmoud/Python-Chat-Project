f887a1187f010b3872236bde6ce386d24a99ee26
Dina1993 <dinaibrahim8.di@gmail.com>
2017-03-15 20:44:37 +0200

    the signup part in handlers

--- handlers/signup.py
+++ handlers/signup.py
@@ -1,7 +1,15 @@
-from tornado import web, websocket
-from pymongo import MongoClient
+from tornado import web,websocket
+import pymongo
 
 
 
 class signup(web.RequestHandler):
-
+	def get(self):
+		self.render("../templates/Loginpage.html")
+	def post(self):
+		client=pymongo.MongoClient()
+		db=client.pro
+		uname=self.get_argument("username")
+		pwd=self.get_argument("pwd")
+		email=self.get_argument("email")
+		db.user.insert({"username":uname,"password":pwd,"email":email})
