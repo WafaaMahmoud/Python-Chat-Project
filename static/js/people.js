0515c1914cebff8212d8d56d88bff733d394d432
DohaEssam <dohaeseldin@gmail.com>
2017-03-15 18:11:20 +0200

    people ja added

--- static/js/people.js
+++ static/js/people.js
@@ -1,3 +1,41 @@
-/**
- * Created by doha on 3/15/17.
- */
+var count=0;
+var index=0;
+    $.ajax({
+
+        url: 'http://localhost:8888/pe',
+        type: "post",
+        success: function (data) {
+            successmessage = 'Data was succesfully captured';
+            for (x in data[0]){
+                //$("#myAllfriends").append("</br>"+data[1][x].username+"</br>");
+
+            }
+
+            for (x in data[0]){
+                $("#myfriends").append("</br>"+data[0][x].friends+"</br>");
+
+            }
+            for (x in data[2]){
+
+                if(data[2][x].numberOfColors>count){
+                    //console.log(data[2][x].numberOfColors);
+                    count=data[2][x].numberOfColors;
+                    index=x;
+                }
+
+
+            }
+
+            $("#myAllfriends").append("</br>"+data[2][index].username+"</br>");
+
+
+
+        },
+        error: function (data) {
+            successmessage = 'Error';
+            $("#myAllfriends").text(successmessage);
+        },
+    });
+
+
+
