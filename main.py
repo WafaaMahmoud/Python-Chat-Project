36a5eb8a4f365782ead5014a19871b0789f0e828
Dina1993 <dinaibrahim8.di@gmail.com>
2017-03-15 20:47:14 +0200

    shilt el commments

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from handlers.ajax import APIHandler
 from handlers.sse import EventHandler,SSEHandler
 from handlers.login import ChatHandler,WSHandler,Login
 from handlers.people import people
-#from handlers.signup import signup
+from handlers.signup import signup
 #from handlers.group import group
-#from handlers.homepage import homepage
+from handlers.homepage import homepage
 
 
 class MainHandler(web.RequestHandler):
     def get(self):
-        self.render("templates/chat.html")
+        self.render("templates/homepage.html")
 
 app = web.Application([
 	(r"/", MainHandler),
@@ -19,8 +19,8 @@ app = web.Application([
         (r"/event", EventHandler),
         (r"/chat", ChatHandler),
         (r"/log", Login),
-        #(r"/singup", signup),
-        (r"/people", people),
+        (r"/signup", signup),
+        #(r"/people", people),
         #(r"/group", group),
         #("/homepage", homepage),
         (r"/ws", WSHandler)
